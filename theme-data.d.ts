8bcf8c1d62c812d1d44a707f7b02456acdac1b86
Dinh Bang <ndinhbang111@gmail.com>
2023-05-06 22:30:18 +0700

    0.0.1

---
+++ theme-data.d.ts
@@ -0,0 +1,5 @@
+import { Theme } from './theme';
+export declare const devuiLightTheme: Theme;
+export declare const devuiGreenTheme: Theme;
+export declare const devuiDarkTheme: Theme;
+export declare const devuiGreenDarkTheme: Theme;
